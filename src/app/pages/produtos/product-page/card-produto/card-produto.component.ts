fd5096d2f5da86d59ccc58b368314052f40cc30f
evan <evandromariano49@gmail.com>
2023-09-26 09:49:46 -0300

    2023-09-26 - card-produto-component

---
+++ src/app/pages/produtos/product-page/card-produto/card-produto.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-produto',
+  templateUrl: './card-produto.component.html',
+  styleUrls: ['./card-produto.component.css']
+})
+export class CardProdutoComponent {
+
+}
--- src/app/pages/produtos/produtos.module.ts
+++ src/app/pages/produtos/produtos.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
 import { ProductPage } from './product-page/product-page.component';
+import { CardProdutoComponent } from './product-page/card-produto/card-produto.component';
 
 @NgModule({
-  declarations: [ProductPage],
+  declarations: [ProductPage, CardProdutoComponent],
   exports: [ProductPage],
   imports: [CommonModule],
 })
