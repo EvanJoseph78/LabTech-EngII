b88213e83073759e40a2a8280566ead94aa9b57a
evan <evandromariano49@gmail.com>
2023-09-27 23:36:35 -0300

    produto modulo corrigido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,17 @@ import { AppComponent } from './app.component';
 import { IndexModule } from './pages/index/index.module';
 import { ProdutosModule } from './pages/produtos/produtos.module';
 import { MenuBarComponent } from './shared/menu-bar/menu-bar.component';
+import { ProdutoModule } from './pages/produto/produto.module';
 
 @NgModule({
   declarations: [AppComponent, MenuBarComponent],
-  imports: [BrowserModule, AppRoutingModule, IndexModule, ProdutosModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    IndexModule,
+    ProdutosModule,
+    ProdutoModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
