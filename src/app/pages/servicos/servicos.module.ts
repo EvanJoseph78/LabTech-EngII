157e4e5b28e120969e507007b7cefee1be9c9eac
evan <evandromariano49@gmail.com>
2023-11-03 10:52:25 -0300

    adiconado página de serviços

---
+++ src/app/pages/servicos/servicos.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ServicosComponent } from './servicos/servicos.component';
+
+
+
+@NgModule({
+  declarations: [
+    ServicosComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ServicosModule { }
---
+++ src/app/pages/servicos/servicos/servicos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servicos',
+  templateUrl: './servicos.component.html',
+  styleUrls: ['./servicos.component.css']
+})
+export class ServicosComponent {
+
+}
