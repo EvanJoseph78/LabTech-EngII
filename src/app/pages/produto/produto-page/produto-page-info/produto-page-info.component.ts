df9c3848ec80bc2e5b1aa638cffd86f0a72b8783
evan <evandromariano49@gmail.com>
2023-11-07 22:21:32 -0300

    categoria produtos e cor produtos arrumado

--- src/app/pages/produto/produto-page/produto-page-info/produto-page-info.component.ts
+++ src/app/pages/produto/produto-page/produto-page-info/produto-page-info.component.ts
@@ -12,4 +12,10 @@ export class ProdutoPageInfoComponent {
   precoProduto: string = '';
   @Input()
   imagemProduto: string = '';
+
+  carregarImagemPadrao() {
+    console.log('Evandroo');
+    this.imagemProduto =
+      'https://www.shutterstock.com/shutterstock/photos/2059817444/display_1500/stock-vector-no-image-available-photo-coming-soon-illustration-vector-2059817444.jpg';
+  }
 }
