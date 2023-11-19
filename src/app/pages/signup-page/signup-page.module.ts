f869ba9894c73b9ad226915c3ad7c6e46dcbdce9
Evan Joseph <evandromariano49@gmail.com>
2023-11-18 23:24:43 -0300

    criada página de criar conta

---
+++ src/app/pages/signup-page/signup-page.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SignupPageComponent } from './signup-page/signup-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    SignupPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SignupPageModule { }
