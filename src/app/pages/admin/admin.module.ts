8c37e835d9cbc8d75dcf2ecdc9639ae62d0eec8c
Evan Joseph <evandromariano49@gmail.com>
2023-11-26 19:32:37 -0300

    melhorado css cadastro component

--- src/app/pages/admin/admin.module.ts
+++ src/app/pages/admin/admin.module.ts
@@ -2,19 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin/admin.component';
 import { CadastroComponent } from './admin/cadastro/cadastro.component';
-
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AdminComponent,
-    CadastroComponent
-  ],
-  exports: [
-    AdminComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [AdminComponent, CadastroComponent],
+  exports: [AdminComponent],
+  imports: [CommonModule, FormsModule],
 })
 export class AdminModule { }
--- src/app/pages/admin/admin/cadastro/cadastro.component.ts
+++ src/app/pages/admin/admin/cadastro/cadastro.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.css']
+  styleUrls: ['./cadastro.component.css'],
 })
 export class CadastroComponent {
-
+  @Input()
+  categoria: string = '';
+  @Input()
+  descricao: string = '';
+  @Input()
+  idproduto: string = '';
+  @Input()
+  nome: string = '';
+  @Input()
+  peso: string = '';
+  @Input()
+  quantidade: string = '';
+  @Input()
+  tamanho: string = '';
+  @Input()
+  urlimg: string = '';
+  @Input()
+  valor: string = '';
 }
